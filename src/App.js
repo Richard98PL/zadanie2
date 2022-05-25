e32ed5af3785154037183e69536eb678eb001267
Richard98PL <41301282+Richard98PL@users.noreply.github.com>
2022-05-25 17:46:54 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit 666 <code>src/App.js</code> and save to reload.
+          Ryszard Rogalski - zadanie 2
         </p>
         <a
           className="App-link"
